74929c4de78297fef5809d5008737679dbe8a939
quentinnicolle <qnicolle@me.com>
2018-11-11 16:46:46 +0100

    :sparkles: setup  Travis CI

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,8 @@
 module.exports = {
   googleClientID:
-    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
-  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
-  cookieKey: '123123123'
+    "964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com",
+  googleClientSecret: "KnH-rZC23z4fr2CN4ISK4srN",
+  mongoURI: "mongodb://quentin:Potter31@ds219532.mlab.com:19532/blog_dev",
+  redisURL : 'redis://127.0.01:6379',
+  cookieKey: "123123123"
 };
---
+++ tests/helpers/page.js
@@ -0,0 +1,75 @@
+const puppeteer = require('puppeteer')
+const sessionFactory = require("../factories/sessionFactory");
+const userFactory = require("../factories/userFactory");
+
+class Page {
+    static async build(){
+        const browser = await puppeteer.launch({
+            headless: true,
+            args: ['--no-sandbox']
+        })
+
+        const page = await browser.newPage()
+        const customPage = new Page(page)
+        
+        return new Proxy(customPage, {
+            get : (target, property) => {
+                return customPage[property] || browser[property] || page[property]
+            }
+        })
+    }
+
+    constructor(page){
+        this.page = page
+    }
+
+    async login() {
+        const user = await userFactory()
+        const { session, sig } = sessionFactory(user)
+
+        await this.page.setCookie({ name: 'session', value: session })
+        await this.page.setCookie({ name: 'session.sig', value: sig })
+        await this.page.goto('http://localhost:3000/blogs')
+        await this.page.waitFor('a[href="/auth/logout"]')
+    }
+
+    async getContentsOf(selector){
+        return this.page.$eval(selector, el => el.innerHTML)
+    }
+
+    get(path){
+        return this.page.evaluate(_path => {
+            return fetch(_path, {
+                method: "GET",
+                credentials: "same-origin",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+            }).then(res => res.json())
+        }, path)
+    }
+
+    post(path, payload){
+        return this.page.evaluate((_path, _payload) => {
+            return fetch(_path, {
+                method: "POST",
+                credentials: "same-origin",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(_payload)
+            }).then(res => res.json())
+        }, path, payload)
+
+    }
+
+    execRequests(actions){
+        return Promise.all(
+            actions.map( ({ method, path, data }) => {
+                return this[method](path, data)
+            })
+        )
+    }
+}
+
+module.exports = Page
